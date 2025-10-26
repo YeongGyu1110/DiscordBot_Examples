e6cfdfe4b9d73d22917f3ab8b6193e689212ac1a
yeonggyu1110 <yeonggyu1110.work@gmail.com>
2025-10-27 05:31:38 +0900

    한글분석기

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
-const client = new Client({ intents: [Guilds, GuildMessages, MessageContent] });
-const { token } = require('./config.js');
-
-client.login(token)
-
-client.once('clientReady', () => console.log('start!'))
